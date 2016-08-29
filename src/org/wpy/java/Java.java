1120fff86f764b89507cd796cf3a78547a60ad34
wpydaguan <1192004829@qq.com>
2016-08-29 16:17:46 +0800

    Update Java.java
    更新一下

--- src/org/wpy/java/Java.java
+++ src/org/wpy/java/Java.java
@@ -3,7 +3,7 @@ package org.wpy.java;
 public class Java {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("世界，Hello World!");
 		System.out.println("我是大官!");
 	}
 }
